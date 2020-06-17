3a9a97d4e27ce911eb479aa90847eb0c8b2105a8
wdjsdev <github.com/wdjsdev>
2020-06-17 11:34:42 -0500

    updated logic to get save location
    also added a button to update save location. removed superfluous reference to includeComponents function

--- components/init_save_loc.js
+++ components/init_save_loc.js
@@ -16,15 +16,6 @@
 function initSaveLoc()
 {
 	log.h("Beginning execution of initSaveLoc()");
-	var saveLoc;
-	var prefPath = documentsPath + "/build_mockup_prefs/"
-	var saveLocPrefFile = File(prefPath + "save_loc_pref.txt");
-	var saveLocPrefFolder = Folder(prefPath);
-	if(!saveLocPrefFolder.exists)
-	{
-		saveLocPrefFolder.create();
-		log.l("Created a saveLocPrefFolder");
-	}
 	
 	saveLocPrefFile.open("r");
 	saveLoc = saveLocPrefFile.read();
@@ -32,26 +23,8 @@ function initSaveLoc()
 
 	if(saveLoc === "")
 	{
-		var w = new Window("dialog");
-			var msg = UI.static(w,"Please choose the location where you would like");
-			var msg2 = UI.static(w,"all of your job folders to be automatically saved.");
-			var btn = UI.button(w,"Ok",function()
-			{
-				w.close();
-			})
-		w.show();
-		saveLoc = desktopFolder.selectDlg("Select a default save location for your orders.").fsName;
-		writeDatabase(saveLocPrefFile.fullName,saveLoc);
+		getSaveLocation();
 	}
 
-	if(!saveLoc)
-	{
-		localJobFolder = desktopFolder;
-	}
-	else
-	{
-		localJobFolder = Folder(saveLoc.replace(/.*users/i,"/Volumes/Macintosh HD/Users/"));
-	}
-	
-
+	log.l("saveLoc = " + saveLoc);
 }
