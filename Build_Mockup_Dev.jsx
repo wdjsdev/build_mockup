426d0fb44df731cb5a454050f9176325ee070a51
wdjsdev <wdjsdev@gmail.com>
2020-06-26 11:54:25 -0500

    cleanup of old components and obsolete versions. converting beta to alpha

--- Build_Mockup_Dev.jsx
+++ Build_Mockup_Dev.jsx
@@ -2,170 +2,143 @@
 function BuildMockup()
 {
 	var valid = true;
-	var scriptName = "build_mockup";
+	var scriptName = "build_mockup_beta";
 
-	eval("#include \"/Volumes/Customization/Library/Scripts/Script Resources/Data/Utilities_Container.jsxbin\"");
-
-	logDest.push(getLogDest());
 	
-	/*****************************************************************************/
+	function getUtilities()
+	{
+		var result = [];
+		var utilPath = "/Volumes/Customization/Library/Scripts/Script_Resources/Data/";
+		var ext = ".jsxbin"
 
-	///////Begin/////////
-	///Logic Container///
-	/////////////////////
+		//check for dev utilities preference file
+		var devUtilitiesPreferenceFile = File("~/Documents/script_preferences/dev_utilities.txt");
 
-		//sendErrors Function Description
-		//Display any errors to the user in a preformatted list
-		function sendErrors(errorList)
+		if(devUtilitiesPreferenceFile.exists)
 		{
-			var errorString = "The Following Errors Occurred:\n";
-			errorString += errorList.join("\n");
-			alert(errorString);
-		}
+			devUtilitiesPreferenceFile.open("r");
+			var prefContents = devUtilitiesPreferenceFile.read();
+			devUtilitiesPreferenceFile.close();
 
-		//sendMsgs Function Description
-		//Display any non-error messages to the user
-		function sendMsgs(msgList)
-		{
-			var msgString = "Non-Error Script Messages:\n";
-			msgString += msgList.join("\n");
-			alert(msgString);
-		}
-
-
-		//testFunction Function Description
-		//run a specific test on a given function
-		//Just manually pass the values that would be expected
-		function testFunction()
-		{
-			//testing getSport function
-			var localValid;
-			log.h("Beginning test function.");
-
-			if(!designNumber)
+			if(prefContents === "true")
 			{
-				getDesignNumber();
-			}
-			else
-			{
-				lib.designNumber = designNumber;
-			}
-			getJson(lib.designNumber);
-			var valid = preValidateJson(lib.json);
-			if(valid)
-			{
-				for(var prop in lib.json)
-				{
-					if(prop.indexOf("config")>-1)
-					{
-						localValid = verifyGarInfo(lib.json[prop],prop);
-						if(localValid)
-						{
-							
-						}
-					}
-				}
-			}
-
-			return localValid;
-		}
-
-		var devPath = "~/Desktop/automation/build_mockup/components";
-		var prodPath = "/Volumes/Customization/Library/Scripts/Script Resources/components/mockup_builder/";
-
-		var componentFiles = includeComponents(devPath,prodPath);
-
-		if(componentFiles)
-		{
-			for(var f=0;f<componentFiles.length;f++)
-			{
-				var thisComponent = componentFiles[f];
-				eval("#include \"" + thisComponent + "\"");
+				utilPath = "~/Desktop/automation/utilities/";
+				ext = ".js";
 			}
 		}
-		else
-		{
-			valid = false;
-		}
 
-
-
-	////////End//////////
-	///Logic Container///
-	/////////////////////
-
-	/*****************************************************************************/
-
-	///////Begin////////
-	////Data Storage////
-	////////////////////
-
-		//global variables
-
-		//non-error messages
-		var msgList = [];
-
-		//general library information
-		//this is agnostic of the garments involved
-		//it just stores static paths to network folders
-		//and the returned design number for the current job
-		var lib = 
-		{
-			designNumbers : [], 
-			designFilePath : "",
-			subGraphicsPath : "",
-			subNameNumbersPath : "",
-			patternFillsPath : "",
-			mascotsPath : "",
-			prepressPath : "",
-			json : {},
-			validated : {},
-			filesToOpen: [],
-			mockupsToOpen: [],
-			curatedMockups: [],
-			graphicsToOpen: [],
-			curatedGraphics: [],
-			reversible: false,
-			adult:undefined,
-			youth:undefined,
-			possibleGarments : ["top", "bottom"],
-			possibleSizes : ["adult", "youth"],
-			sports : ["ACCESSORIES","BAGS","BASKETBALL","COMPRESSION","DIAMOND SPORTS","FOOTBALL","FOOTBALL 7 ON 7","LACROSSE","SOCCER","SPIRITWEAR","VOLLEYBALL"]
-		}
-
-		//set some file path constants
 		if($.os.match("Windows"))
 		{
-			//PC
-			
-			// lib.designFilePath = "\"N:\\Library\\Scripts\\Script Resources\\Data\\json_design_files";
-			errorList.push("Sorry, this script doesn't currently work on PC.");
-			log.e("User is on a PC.::This script is not yet designed to work on PC.");
-			sendErrors(errorList);
-			printLog();
-			valid = false;
-		} 
-		else 
-		{
-			// MAC
-			lib.designFilePath = "/Volumes/Customization/Library/Scripts/Script Resources/Data/json_design_files";
-			lib.subGraphicsPath = "/Volumes/Customization/Library/Graphics/Sublimation/";
-			lib.subNameNumbersPath = "/Volumes/Customization/Library/Graphics/Sublimation_Name_Numbers/";
-			lib.authenticPressNumbersPath = "/Volumes/Customization/Library/Graphics/Authentics/NUMBERS/_SCREEN PRESSED/";
-			lib.authenticSPGraphicsPath = "/Volumes/Customization/Library/Graphics/Authentics/LOGOS/_SCREEN PRINT/";
-			lib.authenticTWGraphicsPath = "/Volumes/Customization/Library/Graphics/Authentics/LOGOS/_TWILL/";
-			lib.authenticTWNumbersPath = "/Volumes/Customization/Library/Graphics/Authentics/NUMBERS/_TWILL/";
-			lib.authenticEMBGraphicsPath = "/Volumes/Customization/Library/Graphics/Authentics/LOGOS/_EMBROIDERY/";
-			lib.patternFillsPath = "/Volumes/Customization/Library/Pattern Fills/";
-			lib.mascotsPath = "/Volumes/Customization/Library/Mascots/";
-			lib.ghostedMascotsPath = "/Volumes/Customization/Library/Mascots/_Ghosted Mascots/";
-			lib.prepressPath = "/Volumes/Customization/Library/cads/prepress/";
+			utilPath = utilPath.replace("/Volumes/","//AD4/");
 		}
 
-		
+		result.push(utilPath + "Utilities_Container" + ext);
+		result.push(utilPath + "Batch_Framework" + ext);
+		return result;
+
+	}
+
+	var utilities = getUtilities();
+	if(utilities)
+	{
+		for(var u=0,len=utilities.length;u<len;u++)
+		{
+			eval("#include \"" + utilities[u] + "\"");	
+		}
+	}
+	else
+	{
+		alert("Failed to find the utilities..");
+		return false;	
+	}
+
+
+
+
+	//get the components
+	var devComponents = desktopPath + "automation/build_mockup/components";
+	var prodComponents = componentsPath + "build_mockup_beta";
+
+	var compFiles = includeComponents(devComponents,prodComponents,false);
+	if(compFiles.length)
+	{
+		var curComponent;
+		for(var cf=0,len=compFiles.length;cf<len;cf++)
+		{
+			curComponent = compFiles[cf].fullName;
+			eval("#include \"" + curComponent + "\"");
+			log.l("included: " + compFiles[cf].name);
+		}
+	}
+	else
+	{
+		errorList.push("Failed to find the necessary components.");
+		log.e("No components were found.");
+		valid = false;
+		return valid;
+	}
+
+	////////////////////////
+	////////ATTENTION://////
+	//
+	//		temporary live logging
+	//		turn off before distribution
+	//
+	////////////////////////	
+
+	LIVE_LOGGING = false;
+	// DEV_LOGGING = true;
+
+	////////////////////////
+	////////ATTENTION://////
+	//
+	//		temporary live logging
+	//		turn off before distribution
+	//
+	////////////////////////	
+
+	//Global Variables
+	var orderNumber;
+	var teamName;
+	var orderData;
+	var garmentsNeeded = [];
+	var curGarmentIndex = 1;
+	var designNumbers = [];
+
+	var womensCodePat = /w$/i;
+	var youthCodePat = /y$/i;
+	var girlsCodePat = /g$/i;
+
+	var saveLoc;
+	var localJobFolder;
+	var curOrderFolder;
+	var currentMockup;
+	var filesToClose = [];
+
+	//regex to remove superfluous appendages from graphic codes
+	//for example, the builder occasionally uses a code like: FDS-325LS
+	//even though the graphic is identical to FDS-325
+	//the builder needs to label the graphic codes this way to facilitate
+	//grouping certain graphics together for use on specific garments..
+	//but for our purposes.. we don't want to do that in art. identical
+	//graphics should have identical codes.
+	var vestigialAppendagePat = /bg|cb|pnt|g|ll|ls|rl|rs|b$/i;
+
+	//regex for name and number graphics
+	//the graphic code will come through with one or the other
+	//but the files are named with both prefixes.
+	//for example, a number code might be FDSN-1001...
+	//but the artwork file for that number will include the player name
+	//graphic as well. As such the file would be named FDSP-FDSN_1001
+	//so the graphic code needs to be updated to include both prefixes
+	var nameNumberPat = /(fdsn|fdsp)[-_]/i;
+
 
 	//mid/garment relationship database
 	var MGR = midGarmentRelationshipDatabasePath = dataPath + "build_mockup_data/mid_garment_relationship_database.js";
+
+
+
 
 	//known converted_template folder locations database
 	//database to keep track of the exact folder locations for a given
@@ -173,42 +146,33 @@ function BuildMockup()
 	var CTFL = convertedTemplateFolderLocationsDatabasePath = dataPath + "build_mockup_data/converted_template_locations_database.js";
 	// var CTFL = convertedTemplateFolderLocationsDatabasePath = desktopPath + "temp/converted_template_locations_database.js";
 
+
+
+
 	//pattern id-style number relationships
 	var PSN = patternIdStyleNumberRlationshipsDatabasePath = dataPath + "build_mockup_data/pattern_id_style_nymber_database.js";
 	// var PSN = patternIdStyleNumberRlationshipsDatabasePath = desktopPath + "temp/build_mockup_data/pattern_id_style_nymber_database.js";
 
+
+
+
 	//known graphic folder locations database
 	//database to keep track of exact folder locations for a given graphic
 	// var GFL = grahpicFolderLocationsDatabasePath = desktopPath + "temp/graphic_locations_database.js";
-	var GFL = grahpicFolderLocationsDatabasePath = dataPath + "build_mockup_data/graphic_locations_database.js";
+	var GFL = grahpicFolderLocationsDatabasePath = dataPath + "build_mockup_data/graphic_folder_locations_database.js";
+	// var GFL = grahpicFolderLocationsDatabasePath = desktopPath + "automation/build_mockup/resources/graphic_folder_locations_database.js";
 
-	var tempPatternFillPath = libraryPath + "";
+	var GLS = graphicLocationAndSizingDatabasePath = dataPath + "build_mockup_data/graphic_locations_and_sizing_database.js";
+
 
 	//
 	//Gather and include the components
 	//
 
 	logDest.push(getLogDest())
-	initLog();
+	// initLog();
 
-	var devComponents = desktopPath + "automation/build_mockup/components";
-	var prodComponents = componentsPath + "build_mockup";
-
-	var compFiles = includeComponents(devComponents,prodComponents,true);
-	if(compFiles)
-	{
-		for(var cf=0,len=compFiles.length;cf<len;cf++)
-		{
-			eval("#include \"" + compFiles[cf] + "\"");
-		}
-	}
-	else
-	{
-		errorList.push("Failed to find the necessary components.");
-		log.e("Failed to find the components.");
-		valid = false;
-		return valid;
-	}
+	
 	
 
 	////////////////////////
@@ -224,6 +188,15 @@ function BuildMockup()
 	//
 	//
 
+	//initialize the save location preference file
+	var prefPath = documentsPath + "build_mockup_prefs/"
+	var saveLocPrefFile = File(prefPath + "save_loc_pref.txt");
+	var saveLocPrefFolder = Folder(prefPath);
+	if(!saveLocPrefFolder.exists)
+	{
+		saveLocPrefFolder.create();
+		log.l("Created a saveLocPrefFolder");
+	}
 
 	//
 	//Gather the order data
@@ -267,7 +240,10 @@ function BuildMockup()
 		loopGarmentsNeeded();
 	}
 
-	garmentsNeeded[0].mockupDocument.activate();
+	if(garmentsNeeded.length && garmentsNeeded[0].mockupDocument)
+	{
+		garmentsNeeded[0].mockupDocument.activate();
+	}
 
 	for(var ftc = filesToClose.length - 1; ftc>=0; ftc--)
 	{
@@ -276,7 +252,7 @@ function BuildMockup()
 		filesToClose[ftc].close(SaveOptions.DONOTSAVECHANGES);
 	}
 	
-	endLog();
+	// endLog();
 	printLog();
 	if(errorList.length)
 	{
