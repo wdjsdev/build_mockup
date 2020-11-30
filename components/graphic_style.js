4f4d503c1e4b927047baee68d04dc72234a556d2
wdjsdev <wdjsdev@gmail.com>
2020-11-30 14:42:18 -0600

    bug fix.
    if no pattern file was located, or pattern swatch found in document, a runtime error occurred. just added error handling for that eventuality.

--- components/graphic_style.js
+++ components/graphic_style.js
@@ -90,134 +90,153 @@
 				this.patternFolder = Folder("/Volumes/Customization/Library/Graphics/Pattern Fills/")
 				this.sourceFile = getFile(this.patternFolder,this.patternStyleNumber,"DSPATTERN-"+ this.patternStyleNumber);
 
-				app.open(this.sourceFile);
 
-				this.doc = app.activeDocument;
-				
-				this.swatches = this.doc.swatches;
-				this.backgroundColor = BUILDER_COLOR_CODES[data.colorCode];
-
-				//variables to hold the above pattern or below pattern layers
-				var gbp,gap;
-
-				var livePatternLayer = findSpecificLayer(this.doc.layers,"Live Pattern");
-				if(!livePatternLayer)
+				if(this.sourceFile)
 				{
-					errorList.push("This pattern is not yet optimized for this script.");
-					log.e("Pattern file does not contain a Pattern layer");
-					filesToClose.push(this.doc);
-					return;
-				}
-				else
-				{
-					//normalize the sublayer names to preclude spelling issues such as "bellow"...
-					//loop the livePatternLayer's layers and fix the names
-					var curLay;
-					var belPat = /^.*_bel.*_.*/i;
-					var aboPat = /^.*_abo.*_.*/i
-					for(var n=0,len=livePatternLayer.layers.length;n<len;n++)
+					app.open(this.sourceFile);
+					this.doc = app.activeDocument;
+					
+					this.swatches = this.doc.swatches;
+					this.backgroundColor = BUILDER_COLOR_CODES[data.colorCode];
+
+					//variables to hold the above pattern or below pattern layers
+					var gbp,gap;
+
+					var livePatternLayer = findSpecificLayer(this.doc.layers,"Live Pattern");
+					if(!livePatternLayer)
 					{
-						curLay = livePatternLayer.layers[n];
-						curLay.name = curLay.name.replace(belPat,"Gradient_Below_Pattern");
-						curLay.name = curLay.name.replace(aboPat,"Gradient_Above_Pattern");
-						
-						if(belPat.test(curLay.name))
-						{
-							gbp = curLay;
-						}
-						if(aboPat.test(curLay.name))
-						{
-							gap = curLay;
-						}
-					}
-				}
-
-
-				var gradientLayer;
-
-				//both pattern AND gradient
-				if(data.pattern && data.gradient)
-				{
-					if(data.gradient.top)
-					{
-						this.targetBlock = gap.pageItems[data.gradient.id];
+						errorList.push("This pattern is not yet optimized for this script.");
+						log.e("Pattern file does not contain a Pattern layer");
+						filesToClose.push(this.doc);
+						return;
 					}
 					else
 					{
-						this.targetBlock = gbp.pageItems[data.gradient.id];
-					}
-				}
-
-				//only a gradient
-				else if(data.gradient)
-				{
-					this.targetBlock = livePatternLayer.layers["No_Pattern"].pageItems[data.gradient.id];
-				}
-				
-				// only a pattern
-				else if(data.pattern)
-				{
-					try
-					{
-						this.targetBlock = livePatternLayer.pageItems["no_gradient"];
-					}
-					catch(e)
-					{
-						this.targetBlock = livePatternLayer.pathItems.rectangle(103,710,152,152);
-						this.targetBlock.stroked = false;
-						this.targetBlock.fillColor = makeNewSpotColor(this.backgroundColor).color;
-						this.targetBlock.selected = true;
-						createAction("add_new_fill",ADD_NEW_FILL_ACTION_STRING);
-						app.doScript("add_new_fill","add_new_fill");
-						removeAction("add_new_fill");
-						var patternSwatch;
-						var curSwatch;
-						for(var p = app.activeDocument.swatches.length - 1; p>=0 && !patternSwatch; p--)
+						//normalize the sublayer names to preclude spelling issues such as "bellow"...
+						//loop the livePatternLayer's layers and fix the names
+						var curLay;
+						var belPat = /^.*_bel.*_.*/i;
+						var aboPat = /^.*_abo.*_.*/i
+						for(var n=0,len=livePatternLayer.layers.length;n<len;n++)
 						{
-							curSwatch = app.activeDocument.swatches[p];
-							if(curSwatch.name.indexOf("DSPATTERN-") === 0)
+							curLay = livePatternLayer.layers[n];
+							curLay.name = curLay.name.replace(belPat,"Gradient_Below_Pattern");
+							curLay.name = curLay.name.replace(aboPat,"Gradient_Above_Pattern");
+							
+							if(belPat.test(curLay.name))
 							{
-								patternSwatch = curSwatch;
+								gbp = curLay;
+							}
+							if(aboPat.test(curLay.name))
+							{
+								gap = curLay;
 							}
 						}
-						
-						this.targetBlock.fillColor = patternSwatch.color;
-						this.targetBlock.name = "no_gradient";
 					}
-				}
 
-				//just a solid fill color
+
+					var gradientLayer;
+
+					//both pattern AND gradient
+					if(data.pattern && data.gradient)
+					{
+						if(data.gradient.top)
+						{
+							this.targetBlock = gap.pageItems[data.gradient.id];
+						}
+						else
+						{
+							this.targetBlock = gbp.pageItems[data.gradient.id];
+						}
+					}
+
+					//only a gradient
+					else if(data.gradient)
+					{
+						this.targetBlock = livePatternLayer.layers["No_Pattern"].pageItems[data.gradient.id];
+					}
+					
+					// only a pattern
+					else if(data.pattern)
+					{
+						try
+						{
+							this.targetBlock = livePatternLayer.pageItems["no_gradient"];
+						}
+						catch(e)
+						{
+							this.targetBlock = livePatternLayer.pathItems.rectangle(103,710,152,152);
+							this.targetBlock.stroked = false;
+							this.targetBlock.fillColor = makeNewSpotColor(this.backgroundColor).color;
+							this.targetBlock.selected = true;
+							createAction("add_new_fill",ADD_NEW_FILL_ACTION_STRING);
+							app.doScript("add_new_fill","add_new_fill");
+							removeAction("add_new_fill");
+							var patternSwatch;
+							var curSwatch;
+							var patternCodeRegex = /^dspattern[\s]*[-_]/i;
+							for(var p = app.activeDocument.swatches.length - 1; p>=0 && !patternSwatch; p--)
+							{
+								curSwatch = app.activeDocument.swatches[p];
+								if(patternCodeRegex.test(curSwatch.name) && curSwatch.name.indexOf(this.patternStyleNumber)>-1)
+								{
+									patternSwatch = curSwatch;
+								}
+							}
+							
+							if(patternSwatch)
+							{
+								this.targetBlock.fillColor = patternSwatch.color;
+								this.targetBlock.name = "no_gradient";	
+							}
+							else
+							{
+								errorList.push("Failed to identify the correct pattern swatch for: DSPATTERN-" + this.patternStyleNumber);
+								log.e("Failed to identify the correct pattern swatch for: DSPATTERN-" + this.patternStyleNumber);
+							}
+							
+						}
+					}
+
+					//just a solid fill color
+					else
+					{
+						this.targetBlock = livePatternLayer.pathItems.rectangle(103,710,152,152);
+						this.targetBlock.filled = true;
+						this.targetBlock.stroked = false;
+						this.targetBlock.fillColor = makeNewSpotColor(this.backgroundColor).color;
+					}
+
+
+
+					this.targetBlock.name = data.id;
+					this.doc.selection = null;
+					this.targetBlock.selected = true;
+					app.executeMenuCommand("Inverse menu item");
+					app.cut();
+					this.recolor("C1");
+					this.targetBlock.selected = true;
+					if(this.patternScale)
+					{
+						this.targetBlock.resize(this.patternScale*100,this.patternScale*100,true,true,true,true);
+					}
+					createAction("graphic_style_from_selection",GRAPHIC_STYLE_FROM_SELECTION_ACTION_STRING);
+					app.doScript("graphic_style_from_selection","graphic_style_from_selection");
+					removeAction("graphic_style_from_selection");
+					this.doc.graphicStyles[this.doc.graphicStyles.length-1].name = data.id;
+					filesToClose.push(this.doc);
+					currentMockup.layers[0].locked = false;
+					currentMockup.layers[0].visible = true;
+					var dupTarget = this.targetBlock.duplicate(currentMockup);
+					dupTarget.remove();
+					this.validGraphicStyle = true;
+				}
 				else
 				{
-					this.targetBlock = livePatternLayer.pathItems.rectangle(103,710,152,152);
-					this.targetBlock.filled = true;
-					this.targetBlock.stroked = false;
-					this.targetBlock.fillColor = makeNewSpotColor(this.backgroundColor).color;
+					this.validGraphicStyle = false;
 				}
 
-
-
-				this.targetBlock.name = data.id;
-				this.doc.selection = null;
-				this.targetBlock.selected = true;
-				app.executeMenuCommand("Inverse menu item");
-				app.cut();
-				this.recolor("C1");
-				this.targetBlock.selected = true;
-				if(this.patternScale)
-				{
-					this.targetBlock.resize(this.patternScale*100,this.patternScale*100,true,true,true,true);
-				}
-				createAction("graphic_style_from_selection",GRAPHIC_STYLE_FROM_SELECTION_ACTION_STRING);
-				app.doScript("graphic_style_from_selection","graphic_style_from_selection");
-				removeAction("graphic_style_from_selection");
-				this.doc.graphicStyles[this.doc.graphicStyles.length-1].name = data.id;
-				filesToClose.push(this.doc);
-				currentMockup.layers[0].locked = false;
-				currentMockup.layers[0].visible = true;
-				var dupTarget = this.targetBlock.duplicate(currentMockup);
-				dupTarget.remove();
-				this.validGraphicStyle = true;
+				
 			}
 			else
 			{
