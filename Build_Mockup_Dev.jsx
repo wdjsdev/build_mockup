1816287fe2043b8cdae0a6f9c70072e5354d5ded
wdjsdev@gmail.com <5a8d33b751374be687dcae723f7f8ea48f6af636>
2021-02-02 11:26:44 -0600

    fix, cleanup, and consolidation of recursive function

--- Build_Mockup_Dev.jsx
+++ Build_Mockup_Dev.jsx
@@ -244,7 +244,7 @@ function BuildMockup()
 		// designNumbers.push("2OrTtaFUtfOG");
 
 
-		designNumbers.push("4sKP8pgtfTH7");
+		designNumbers.push("KyT4duiQ1kuB");
 
 		//
 	}
--- components/update_graphic_text.js
+++ components/update_graphic_text.js
@@ -5,39 +5,31 @@
 //labeled "graphic_text_[sequence number]"
 // and update textframe contents accordingly.
 function updateGraphicText(arr, group)
-{
-	var newText;
-	var curItem,arrIndex;
-	for(var x=0;x < group.pageItems.length;x++)
-	{
-		curItem = group.pageItems[x];
-		arrIndex = parseInt(curItem.name.substring(curItem.name.length-1,curItem.name.length)) - 1;
-		if(curItem.typename === "TextFrame" && curItem.name.indexOf("graphic_text")>-1)
-		{
-			newText = arr[arrIndex];
-			inputNewLogoText(curItem,newText);
-		}
-		else if(curItem.typename === "GroupItem")
-		{
-			dig(curItem);
-		}
-	}
+{	
+	
+	dig(group);
 
 
-	var tmpName,arrIndex;
+	
 	function dig(inGroup)
 	{
+		var arrIndex;
+		var tmpName;
+		var curItem;
 		for(var d=0;d<inGroup.pageItems.length;d++)
 		{
-			if(inGroup.pageItems[d].typename === "TextFrame" && inGroup.pageItems[d].name.indexOf("graphic_text")>-1)
+			curItem = inGroup.pageItems[d];
+			log.l("curItem.typename = " + curItem.typename);
+			if(curItem.typename === "TextFrame" && curItem.name.indexOf("graphic_text")>-1)
 			{
-				tmpName = inGroup.pageItems[d].name;
+				tmpName = curItem.name;
 				arrIndex = parseInt(tmpName.substring(tmpName.length-1,tmpName.length)) - 1;
-				inputNewLogoText(inGroup.pageItems[d],arr[arrIndex]);
+				arrIndex = parseInt(tmpName.charAt(tmpName.length-1)) -1;
+				inputNewLogoText(curItem,arr[arrIndex]);
 			}
-			else if(inGroup.pageItems[d].typename === "GroupItem")
+			else if(curItem.typename === "GroupItem")
 			{
-				dig(inGroup);
+				dig(curItem);
 			}
 		}
 	}
