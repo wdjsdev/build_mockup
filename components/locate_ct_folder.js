a001c360bea87ef81df25166e5cb894385ae2958
wdjsdev@gmail.com <5a8d33b751374be687dcae723f7f8ea48f6af636>
2022-09-20 14:50:32 -0500

    Update locate_ct_folder.js
    update logic to accommodate usage on a PC. removed the mac formatted absolute file paths and replaced with paths relative to the library - cads - prepress folder and then utilized the dynamic path to the prepress folder instead. silly me, should have done that long ago.

--- components/locate_ct_folder.js
+++ components/locate_ct_folder.js
@@ -16,10 +16,10 @@
 
 */
 
-function locateCTFolder(mid)
+function locateCTFolder ( mid )
 {
-	log.h("Beginning execution of locateCTFolder(" + mid + ")");
-	var garmentFolder,mockupFolder,gfFiles,ctFolder,exit = false;
+	log.h( "Beginning execution of locateCTFolder(" + mid + ")" );
+	var garmentFolder, mockupFolder, gfFiles, ctFolder, exit = false;
 	var maxDepth = 2;
 	var curDepth = 0;
 
@@ -28,64 +28,64 @@ function locateCTFolder(mid)
 	//include the database
 	try
 	{
-		eval("#include \"" + CTFL + "\"");		
+		eval( "#include \"" + CTFL + "\"" );
 	}
-	catch(e)
+	catch ( e )
 	{
-		var dbFile = File(CTFL);
-		dbFile.open("w");
-		dbFile.write("var ctLocations = {};");
+		var dbFile = File( CTFL );
+		dbFile.open( "w" );
+		dbFile.write( "var ctLocations = {};" );
 		dbFile.close();
-		eval("#include \"" + CTFL + "\"");
+		eval( "#include \"" + CTFL + "\"" );
 	}
-	
-	if(ctLocations[mid])
-	{
-		log.l("Found an entry for " + mid + " in the database.")
-		log.l("CT folder = " + ctLocations[mid]);
 
-		if(ctLocations[mid] === "no_youth_garment" || ctLocations[mid] === "no_mockup")
+	if ( ctLocations[ mid ] )
+	{
+		log.l( "Found an entry for " + mid + " in the database." )
+		log.l( "CT folder = " + ctLocations[ mid ] );
+
+		if ( ctLocations[ mid ] === "no_youth_garment" || ctLocations[ mid ] === "no_mockup" )
 		{
 			ctFolder = undefined;
-			log.l(mid + " is on the no_mockup list.");
+			log.l( mid + " is on the no_mockup list." );
 		}
 		else
 		{
-			ctFolder = Folder(ctLocations[mid]);	
+			ctFolder = Folder( prepressPath + ctLocations[ mid ] );
 		}
-		
+
 	}
 	else
 	{
-		log.l("Nothing in the database for " + mid + ". Beginning to dig through prepress folders.");
+		log.l( "Nothing in the database for " + mid + ". Beginning to dig through prepress folders." );
 
 		//disabling recursive search because it's too expensive time wise
 		//just ask the user where the file is and be done with it.
 		// digForFolders(prepressFolder);
-		
-		if(!ctFolder )
+
+		if ( !ctFolder )
 		{
 			//if it's a youth garment and nothing was found
 			//automatically, assume there is no youth garment
-			if(!youthPat.test(mid))
+			if ( !youthPat.test( mid ) )
 			{
-				ctFolder = prepressFolder.selectDlg("Select Converted Template Folder or Mockup Folder for: " + mid + ".");
+				ctFolder = prepressFolder.selectDlg( "Select Converted Template Folder or Mockup Folder for: " + mid + "." );
 			}
 		}
 
 		//if there is a ct folder, save the folder to the database. 
 		//else, return undefined..
 
-		if(ctFolder)
+		if ( ctFolder )
 		{
 			//write the new ct/mockup folder to the database
-			ctLocations[mid] = ctFolder.fsName;
-			writeDatabase(CTFL,"var ctLocations = " + JSON.stringify(ctLocations));
-			log.l("added {" + mid + ", " + ctFolder.fsName + "} to ctLocations database.");
+			ctLocations[ mid ] = ctFolder.fsName;
+			writeDatabase( CTFL, "var ctLocations = " + JSON.stringify( ctLocations ) );
+			log.l( "added {" + mid + ", " + ctFolder.fsName + "} to ctLocations database." );
 		}
 		else
 		{
-			log.e("Failed to find the proper ct folder..");
+			log.e( "Failed to find the proper ct folder.." );
 		}
 
 
@@ -95,54 +95,54 @@ function locateCTFolder(mid)
 
 
 	//recursive function for finding the proper CT folder in the prepress folder.
-	
-	function digForFolders(loc)
-	{	
-		log.l("Digging for " + mid + " in " + loc.fsName);
-		if(curDepth == 0)
+
+	function digForFolders ( loc )
+	{
+		log.l( "Digging for " + mid + " in " + loc.fsName );
+		if ( curDepth == 0 )
 		{
 			var files = loc.getFiles();
 		}
-		else if(curDepth === 1)
+		else if ( curDepth === 1 )
 		{
-			var files = loc.getFiles("*" + mid + "*");
+			var files = loc.getFiles( "*" + mid + "*" );
 		}
 		else
 		{
 			curDepth--;
 			return;
 		}
-		for(var f=0,len=files.length;f<len && !ctFolder && !exit && curDepth < maxDepth;f++)
+		for ( var f = 0, len = files.length; f < len && !ctFolder && !exit && curDepth < maxDepth; f++ )
 		{
-			if(files[f] instanceof Folder)
+			if ( files[ f ] instanceof Folder )
 			{
-				if(files[f].name.indexOf(mid + "_") > -1)
+				if ( files[ f ].name.indexOf( mid + "_" ) > -1 )
 				{
-					garmentFolder = files[f];
+					garmentFolder = files[ f ];
 					gfFiles = garmentFolder.getFiles();
-					for(var y=0,gfLen=gfFiles.length;y<gfLen;y++)
+					for ( var y = 0, gfLen = gfFiles.length; y < gfLen; y++ )
 					{
-						if(gfFiles[y] instanceof Folder)
+						if ( gfFiles[ y ] instanceof Folder )
 						{
-							if(gfFiles[y].name.toLowerCase().indexOf("onvert")>-1)
+							if ( gfFiles[ y ].name.toLowerCase().indexOf( "onvert" ) > -1 )
 							{
-								ctFolder = gfFiles[y];
-								log.l("Found the CT folder here:");
-								log.l(ctFolder.fsName);
+								ctFolder = gfFiles[ y ];
+								log.l( "Found the CT folder here:" );
+								log.l( ctFolder.fsName );
 							}
-							else if(gfFiles[y].name.toLowerCase().indexOf("ockup")>-1)
+							else if ( gfFiles[ y ].name.toLowerCase().indexOf( "ockup" ) > -1 )
 							{
-								mockupFolder = gfFiles[y];
+								mockupFolder = gfFiles[ y ];
 							}
 						}
 					}
-					if(mockupFolder && !ctFolder)
+					if ( mockupFolder && !ctFolder )
 					{
 						ctFolder = mockupFolder;
-						log.l("No ct folder. using the mockup folder instead.");
-						log.l("mockupFolder.fsName = " + mockupFolder.fsName);
+						log.l( "No ct folder. using the mockup folder instead." );
+						log.l( "mockupFolder.fsName = " + mockupFolder.fsName );
 					}
-					if(!ctFolder)
+					if ( !ctFolder )
 					{
 						exit = true;
 					}
@@ -150,7 +150,7 @@ function locateCTFolder(mid)
 				else
 				{
 					curDepth++;
-					digForFolders(files[f]);
+					digForFolders( files[ f ] );
 				}
 			}
 		}
