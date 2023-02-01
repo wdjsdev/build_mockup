5bd5d05764a2ad0990e0044830d32fb9f36ab6fd
wdjsdev@gmail.com <5a8d33b751374be687dcae723f7f8ea48f6af636>
2023-02-01 10:53:11 -0600

    Squashed commit of the following:
    commit f9f5061d52b055b25bf1b7d6859f1ed23a78f86e
    Author: wdjsdev@gmail.com <5a8d33b751374be687dcae723f7f8ea48f6af636>
    Date:   Wed Feb 1 10:50:44 2023 -0600
    
        Update X_Batch_Build_Mockup_Dev.jsx
    
        fix path to mockup asset folder
    
    commit f2f94d4018baa79be66e6ca750072b827b7566a9
    Author: wdjsdev@gmail.com <5a8d33b751374be687dcae723f7f8ea48f6af636>
    Date:   Mon Jan 30 14:51:16 2023 -0600
    
        Update copy_order_to_asset_folder.js
    
        update path to mockup assets folder

--- components/copy_order_to_asset_folder.js
+++ components/copy_order_to_asset_folder.js
@@ -1,17 +1,17 @@
-function copyOrderToAssetFolder(folderPath)
+function copyOrderToAssetFolder ( folderPath )
 {
 	//trim out any trailing slash to avoid trying to sync 
 	//the job folder with the mockup asset folder.
 	//we only want to put the job folder INSIDE mockup assets.
-	folderPath = folderPath.replace(/\/$/,"");
+	folderPath = folderPath.replace( /\/$/, "" );
 
 
 	const spacePat = /\s/g;
 	const hardSpace = "\\\\ ";
-	var assetFolderPath = "/Volumes/Customization/1_Active Orders/1_Mockup IN PROGRESS/_Mockup_Asset_Folders_/";
-	assetFolderPath = assetFolderPath.replace(spacePat,hardSpace);
-	folderPath = folderPath.replace(spacePat,hardSpace);
+	var assetFolderPath = customizationPath + "1_Mockup_Files/_Mockup_Asset_Folders_/";
+	assetFolderPath = assetFolderPath.replace( spacePat, hardSpace );
+	folderPath = folderPath.replace( spacePat, hardSpace );
 	var shellScriptText = "rsync -vr " + folderPath + " " + assetFolderPath;
-	genericShellScript(shellScriptText);
-	
+	genericShellScript( shellScriptText );
+
 }
